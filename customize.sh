49faff7938c03c8812e28f25312b6bc82ed63379
Majaahh <maja.blaszczak@proton.me>
2026-03-09 00:02:54 +0100

    a53x: rework customize.sh
    Change-Id: I8b38f1a3bbc7d4bc134481544fd3160813be3661
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- customize.sh
+++ customize.sh
@@ -14,46 +14,41 @@ for i in "odm" "vendor" "vendor_dlkm"; do
     unset PROP
 done
 
-LOG_STEP_IN "- Adding NXP NFC Support"
-LOG_STEP_IN "- Deleting NFC blobs"
+# NXP NFC Support
+LOG_STEP_IN "- Deleting SLSI NFC init"
 DELETE_FROM_WORK_DIR "vendor" "etc/init/sec.android.hardware.nfc@1.2-service.rc"
 LOG_STEP_OUT
 
-LOG_STEP_IN "- Adding NXP eSE blobs"
-ADD_TO_WORK_DIR "a53xdcm" "vendor" "etc/libese-nxp.conf"
-ADD_TO_WORK_DIR "a53xdcm" "vendor" "lib64/ese_spi_nxp.so"
-LOG_STEP_OUT
-
-LOG_STEP_IN "- Adding NFC blobs"
+LOG_STEP_IN "- Adding NXP NFC blobs"
 ADD_TO_WORK_DIR "a53xdcm" "vendor" "bin/hw/nxp.android.hardware.nfc@1.2-service"
 ADD_TO_WORK_DIR "a53xdcm" "vendor" "etc/libnfc-nxp.conf"
 ADD_TO_WORK_DIR "a53xdcm" "vendor" "etc/nfc/libnfc-nxp_RF.conf"
 ADD_TO_WORK_DIR "a53xdcm" "vendor" "firmware/nfc/libsn100u_fw.so"
 ADD_TO_WORK_DIR "a53xdcm" "vendor" "lib64/nfc_nci_nxpsn.so"
 
-ADD_TO_WORK_DIR "a53xdcm" "system" "system/etc/libnfc-nci.conf"
+LOG_STEP_IN "- Adding NXP eSE blobs"
+ADD_TO_WORK_DIR "a53xdcm" "vendor" "etc/libese-nxp.conf"
+ADD_TO_WORK_DIR "a53xdcm" "vendor" "lib64/ese_spi_nxp.so"
+LOG_STEP_OUT
 
-LOG "- Renaming /system/system/etc/libnfc-nci.conf to /system/system/etc/libnfc-nci-NXP.conf"
-EVAL "mv \"$WORK_DIR/system/system/etc/libnfc-nci.conf\" \"$WORK_DIR/system/system/etc/libnfc-nci-NXP.conf\""
-sed -i "s/libnfc-nci\\\\.conf/libnfc-nci-NXP\\\\.conf/g" "$WORK_DIR/configs/file_context-system" || return 1
-sed -i "s/libnfc-nci\.conf/libnfc-nci-NXP\.conf/g" "$WORK_DIR/configs/fs_config-system" || return 1
-
+LOG_STEP_IN "- Setting up libnfc-nci configuration"
 ADD_TO_WORK_DIR "$TARGET_FIRMWARE" "system" "system/etc/libnfc-nci.conf" 0 0 644 "u:object_r:system_file:s0"
-
 LOG "- Renaming /system/system/etc/libnfc-nci.conf to /system/system/etc/libnfc-nci-SLSI.conf"
 EVAL "mv \"$WORK_DIR/system/system/etc/libnfc-nci.conf\" \"$WORK_DIR/system/system/etc/libnfc-nci-SLSI.conf\""
-sed -i "s/libnfc-nci\\\\.conf/libnfc-nci-SLSI\\\\.conf/g" "$WORK_DIR/configs/file_context-system" || return 1
-sed -i "s/libnfc-nci\.conf/libnfc-nci-SLSI\.conf/g" "$WORK_DIR/configs/fs_config-system" || return 1
-LOG_STEP_OUT
+SET_METADATA "system" "system/etc/libnfc-nci-SLSI.conf" 0 0 644 "u:object_r:system_file:s0"
+
+ADD_TO_WORK_DIR "a53xdcm" "system" "system/etc/libnfc-nci.conf"
+LOG "- Renaming /system/system/etc/libnfc-nci.conf to /system/system/etc/libnfc-nci-NXP.conf"
+EVAL "mv \"$WORK_DIR/system/system/etc/libnfc-nci.conf\" \"$WORK_DIR/system/system/etc/libnfc-nci-NXP.conf\""
+SET_METADATA "system" "system/etc/libnfc-nci-NXP.conf" 0 0 644 "u:object_r:system_file:s0"
 LOG_STEP_OUT
 
-if ! grep -q "init_31_0 tee_file" "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"; then
-    {
-        echo "(allow init_31_0 tee_file (dir (mounton)))"
-        echo "(allow priv_app_31_0 tee_file (dir (getattr)))"
-        echo "(allow init_31_0 vendor_fw_file (file (mounton)))"
-        echo "(allow priv_app_31_0 vendor_fw_file (file (getattr)))"
-        echo "(allow init_31_0 vendor_npu_firmware_file (file (mounton)))"
-        echo "(allow priv_app_31_0 vendor_npu_firmware_file (file (getattr)))"
-    } >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil"
-fi
+LOG "- Adding SELinux entries"
+{
+    echo "(allow init_31_0 tee_file (dir (mounton)))"
+    echo "(allow priv_app_31_0 tee_file (dir (getattr)))"
+    echo "(allow init_31_0 vendor_fw_file (file (mounton)))"
+    echo "(allow priv_app_31_0 vendor_fw_file (file (getattr)))"
+    echo "(allow init_31_0 vendor_npu_firmware_file (file (mounton)))"
+    echo "(allow priv_app_31_0 vendor_npu_firmware_file (file (getattr)))"
+} >> "$WORK_DIR/vendor/etc/selinux/vendor_sepolicy.cil" || return 1
